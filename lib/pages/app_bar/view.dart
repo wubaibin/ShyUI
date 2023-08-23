1e9ea4d08ae8e68903694a4c8e1d006f59cfa752
吴白彬 <2448428125@qq.com>
2023-08-23 16:45:27 +0800

    feat: 新增AppBar 导航栏

---
+++ lib/pages/app_bar/view.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_face/components/detail_title.dart';
+import 'package:flutter_face/components/stage.dart';
+import 'package:flutter_face/ui/app_bar.dart';
+import 'package:get/get.dart';
+
+import 'index.dart';
+// import 'widgets/widgets.dart';
+
+class AppBarPage extends GetView<AppBarController> {
+  const AppBarPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetBuilder<AppBarController>(
+      builder: (_) {
+        return const StageWidget(
+          title: "AppBar 导航栏",
+          children: [
+            DetailTitleWidget(title: '基础用法', top: 20),
+            SizedBox(height: 12),
+            AppBarWidget(title: "导航栏"),
+            DetailTitleWidget(title: '去掉返回按钮', top: 20),
+            SizedBox(height: 12),
+            AppBarWidget(title: "导航栏", leading: SizedBox()),
+          ],
+        );
+      },
+    );
+  }
+}
