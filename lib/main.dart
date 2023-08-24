4b82505d2de8026e9f6e1d829623791c360d60b4
吴白彬 <2448428125@qq.com>
2023-08-24 17:23:14 +0800

    feat: 去掉不必要的文件

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:get/get.dart';
 import 'package:flutter_face/lang/index.dart';
 import 'package:flutter_face/router/pages.dart';
 import 'package:flutter_face/styles/theme.dart';
-import 'package:flutter_face/utils/storage.dart';
 import 'dart:ui' as ui;
 
 void main() {
@@ -18,30 +17,6 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
-  Locale? locale;
-
-  @override
-  void initState() {
-    super.initState();
-    _disposeLocale();
-  }
-
-  void _disposeLocale() async {
-    final res = await Storage().get("lang");
-    if (res == null) {
-      setState(() {
-        locale = ui.PlatformDispatcher.instance.locale;
-      });
-      return;
-    }
-    final localeValue =
-        res == 'zh_CN' ? const Locale('zh', 'CN') : const Locale('en', 'US');
-    setState(() {
-      locale = localeValue;
-    });
-    Get.updateLocale(localeValue);
-  }
-
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
@@ -50,7 +25,7 @@ class _AppState extends State<App> {
       debugShowCheckedModeBanner: false,
       enableLog: true,
       theme: AppTheme.light,
-      locale: locale ?? ui.PlatformDispatcher.instance.locale,
+      locale: ui.PlatformDispatcher.instance.locale,
       fallbackLocale: TranslationService.fallbackLocale,
       supportedLocales: TranslationService.supportedLocales,
       translations: TranslationService(),
